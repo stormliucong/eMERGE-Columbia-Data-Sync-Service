bd83b883328cef3d3e4adf76484dd7973a8a1a00
Cong Liu <cl3720@aqua.dbmi.columbia.edu>
2023-01-18 11:08:56 -0500

    cron update

--- cron_job.sh
+++ cron_job.sh
@@ -10,3 +10,8 @@
 --log /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/empi_convert.log \
 --token /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/api_tokens.json \
 2> /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/empi_convert_errors.log
+
+/home/cl3720/miniconda3/bin/python /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/get_provider_npi.py \
+--log /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/get_provider_npi.log \
+--token /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/api_tokens.json \
+2> /phi_home/cl3720/phi/eMERGE/eIV-recruitement-support-redcap/get_provider_npi_errors.log
